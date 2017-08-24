cbaf39c7175cc198d7ee71fc47cee1ee4b6cd6c3
Hamid Doust <hamid.doust@gmail.com>
2017-08-24 13:42:50 +0200

    added an commented line which demonstrates the new added feature

--- SRCountdownTimerExample/SRCountdownTimerExample/ViewController.swift
+++ SRCountdownTimerExample/SRCountdownTimerExample/ViewController.swift
@@ -15,11 +15,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // uncomment this line if would like to see a minute and second representation of the remaining time
+        // (rather than just a second representation
+        //countdownTimer.useMinutesAndSecondsRepresentation = true
+        
         countdownTimer.labelFont = UIFont(name: "HelveticaNeue-Light", size: 50.0)
         countdownTimer.labelTextColor = UIColor.red
         countdownTimer.timerFinishingText = "End"
         countdownTimer.lineWidth = 4
-        countdownTimer.start(beginingValue: 10, interval: 1)
+        countdownTimer.start(beginingValue: 15, interval: 1)
+        
     }
 }
 
